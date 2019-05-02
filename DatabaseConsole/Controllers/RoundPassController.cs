aefb6c64a7cc6b13ae9babac5bfa2a877d40a2a9
Michael <micavaleri@gmail.com>
2019-05-02 12:29:29 +0200

    Made some stuff to display stats

--- DatabaseConsole/Controllers/RoundPassController.cs
+++ DatabaseConsole/Controllers/RoundPassController.cs
@@ -47,8 +47,6 @@ namespace DatabaseConsole
         public void CalculateRoundEvents()
         {
             //logik for det som skal ske før IntermediateState påbegyndes
-
-
             this.roundNumber += 1;
             this.amountOfCampers = 30;
 
@@ -217,5 +215,50 @@ namespace DatabaseConsole
             }
         }
 
+        public int GetNumberOfCampersWithAUnitType()
+        {
+            return campers.FindAll((camper) => 
+            {
+                return (camper.UnitTypeId > 0) ? true : false;
+            }).Count;
+        }
+
+        public int GetNumberOfCampersByPlayerID(int player_id)
+        {
+            int theRPID = (player_id == roundPassedForPlayerOne.CampingpladsId) ? roundPassedForPlayerOne.ID : roundPassedForPlayerTwo.ID;
+
+            return campers.FindAll((campist) => 
+            {
+                bool r = false;
+                foreach (RoundPassedUnitTypeModel rput in roundPassedUnitTypes)
+                {
+                    if (rput.CampistID == campist.ID && rput.RoundPassedID == theRPID)
+                    {
+                        r = true;
+                        break;
+                    }
+                }
+                return r;
+            }).Count;
+        }
+
+        public int GetNumberOfCampersByPlayerID(int player_id, string in_which_type)
+        {
+            int theRPID = (player_id == roundPassedForPlayerOne.CampingpladsId) ? roundPassedForPlayerOne.ID : roundPassedForPlayerTwo.ID;
+
+            return campers.FindAll((campist) =>
+            {
+                bool r = false;
+                foreach (RoundPassedUnitTypeModel rput in roundPassedUnitTypes)
+                {
+                    if (rput.CampistID == campist.ID && rput.RoundPassedID == theRPID && campist.Pref == in_which_type)
+                    {
+                        r = true;
+                        break;
+                    }
+                }
+                return r;
+            }).Count;
+        }
     }
 }
--- DatabaseConsole/States/IntermediateState.cs
+++ DatabaseConsole/States/IntermediateState.cs
@@ -36,24 +36,25 @@ namespace DatabaseConsole
 
             // General stats
             Console.WriteLine($"{RoundPassController.Instance.AmountOfCampers} number of campers visited the camping areas.");
-            Console.WriteLine($"{123456} number of these campers found a place they want to rent.");
+            Console.WriteLine($"{RoundPassController.Instance.GetNumberOfCampersWithAUnitType()} number of these campers found a place they want to rent.");
             Console.WriteLine($"A total of {RoundPassController.Instance.RoundPassedForPlayerOne.CampingpladsIncome + RoundPassController.Instance.RoundPassedForPlayerOne.CampingpladsIncome} income was shared between both camping grounds.");
 
             // Player one stats
             Console.WriteLine($"This is what happened in {CampsiteController.Instance.PlayerOne.Title}");
-            Console.WriteLine($"{123456} number of campers chose your campsite.");
-            Console.WriteLine($"{123456} of them found a nice tent.");
-            Console.WriteLine($"{123456} of them prefered your caravans.");
-            Console.WriteLine($"This means you had {123456} empty tents and {123456} empty caravans at the end of the day.");
-            Console.WriteLine($"You earned at total of {123456} today, and have a total balance of {123456} now.");
+            Console.WriteLine($"{RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerOne.ID)} number of campers chose your campsite.");
+            Console.WriteLine($"{RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerOne.ID, "Tent")} of them found a nice tent.");
+            Console.WriteLine($"{RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerOne.ID, "Caravan")} of them prefered your caravans.");
+            Console.WriteLine($"This means you had {RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerOne.ID, "Tent") - CampsiteController.Instance.GetNumberOfUnitsForPlayer(CampsiteController.Instance.PlayerOne.ID, "Tent")} empty tents and {RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerOne.ID, "Caravan") - CampsiteController.Instance.GetNumberOfUnitsForPlayer(CampsiteController.Instance.PlayerOne.ID, "Caravan")} empty caravans at the end of the day.");
+            Console.WriteLine($"You earned at total of {RoundPassController.Instance.RoundPassedForPlayerOne.CampingpladsIncome} today, and have a total balance of {CampsiteController.Instance.PlayerOne.TotalIncome} now.");
 
             // Player two stats
             Console.WriteLine($"This is what happened in {CampsiteController.Instance.PlayerTwo.Title}");
-            Console.WriteLine($"{123456} number of campers chose your campsite.");
-            Console.WriteLine($"{123456} of them found a nice tent.");
-            Console.WriteLine($"{123456} of them prefered your caravans.");
-            Console.WriteLine($"This means you had {123456} empty tents and {123456} empty caravans at the end of the day.");
-            Console.WriteLine($"You earned at total of {123456} today, and have a total balance of {123456} now.");
+            Console.WriteLine($"{RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerTwo.ID)} number of campers chose your campsite.");
+            Console.WriteLine($"{RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerTwo.ID, "Tent")} of them found a nice tent.");
+            Console.WriteLine($"{RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerTwo.ID, "Caravan")} of them prefered your caravans.");
+            Console.WriteLine($"This means you had {RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerTwo.ID, "Tent") - CampsiteController.Instance.GetNumberOfUnitsForPlayer(CampsiteController.Instance.PlayerTwo.ID, "Tent")} empty tents and {RoundPassController.Instance.GetNumberOfCampersByPlayerID(CampsiteController.Instance.PlayerTwo.ID, "Caravan") - CampsiteController.Instance.GetNumberOfUnitsForPlayer(CampsiteController.Instance.PlayerTwo.ID, "Caravan")} empty caravans at the end of the day.");
+            Console.WriteLine($"You earned at total of {RoundPassController.Instance.RoundPassedForPlayerOne.CampingpladsIncome} today, and have a total balance of {CampsiteController.Instance.PlayerTwo.TotalIncome} now.");
+
 
             Console.WriteLine("Press `spacebar` to continue when both players are ready");
 
